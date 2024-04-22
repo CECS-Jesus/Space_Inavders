a5fc0500ee7c1aecda9e76632c5c4946e63d008e
CECS-Jesus <119976553+CECS-Jesus@users.noreply.github.com>
2024-04-22 14:53:10 -0700

    Updated SysTick.c indentation.

--- Src/SysTick.c
+++ Src/SysTick.c
@@ -12,9 +12,9 @@ void SysTick_Init(uint8_t Hz) {
 	NVIC_ST_CURRENT_R = 0;                  		// clear current value register
 	// Set the priority for SysTick interrupt to 0 (highest priority) (bits 13-15)
 	NVIC_SYS_PRI1_R = (NVIC_SYS_PRI1_R&0x1FFF1FFF);
-	NVIC_ST_CTRL_R = NVIC_ST_CTRL_INTEN |   		// enable interrupts
-									 NVIC_ST_CTRL_ENABLE |  		// enable SysTick
-									 NVIC_ST_CTRL_CLK_SRC;  		// use core clock
+	NVIC_ST_CTRL_R = NVIC_ST_CTRL_INTEN | 	// enable interrupts
+	NVIC_ST_CTRL_ENABLE |  			// enable SysTick
+	NVIC_ST_CTRL_CLK_SRC;  			// use core clock
 }
 
 void SysTick_Handler(void){
